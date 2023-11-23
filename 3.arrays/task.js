706f16df4e0347b1b810dcc18f3230c692a3cb95
Denis Fedosov <stalsxl@gmail.com>
2023-11-23 18:25:44 +0300

    homework3 update

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    return arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i]);
+  }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+  function getUsersNamesInAgeRange(users, gender) {
+    const filteredUsers = users.filter((user) => user.gender === gender);
+    if (filteredUsers.length === 0) {
+        return 0;
+    } 
+    const ageSum = filteredUsers.reduce((current, user) => current + user.age, 0);
+    const result = ageSum / filteredUsers.length;
+    return result;
+  }
