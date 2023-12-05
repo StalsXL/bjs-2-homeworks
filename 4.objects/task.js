abbcc46a5a655121c4c92e6f0d7cb1e5fc6288d0
Denis Fedosov <stalsxl@gmail.com>
2023-12-05 19:57:49 +0300

    homework4 update

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
-}
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+};
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+    this.subject = subjectName; 
+};
 
-Student.prototype.addMarks = function (...marks) {
+Student.prototype.addMarks = function (...marksToAdd) {
+    if(this.marks) {
+        this.marks.push(...marksToAdd);
+    }
   
-}
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+    if (this.marks) {
+        return this.marks.reduce((acc,item) => acc + item / this.marks.length, 0);
+    } else {
+        return 0;
+    }    
+
+    
+};
 
 Student.prototype.exclude = function (reason) {
-  
-}
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+};
